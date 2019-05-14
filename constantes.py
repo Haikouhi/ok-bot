4b915339fc9b16c2b621cd45b25900ad2b7c3b40
theoflaus <flaus.theo69@gmail.com>
2019-05-14 16:09:41 +0200

    Modif code

---
+++ constantes.py
@@ -0,0 +1,15 @@
+word_dict = {
+    "salut": ["salut", "hey", "yo", "wesh", "coucou", "bonjour"],
+    "age": ["age"],
+    "mail": ["e-mail", "mail"],
+    "adresse": ["ville", "adresse", "habite", "l'adresse"],
+    "date": ["date", "naissance"],
+    "signe": ["signe", "astrologique"],
+    "ça_va": ["comment", "ça", "vas", "bien", "va", "ca"],
+    "anniversaire": ["anniversaire", "l'anniversaire"],
+    "horoscope": ["horoscope", "l'horoscope"],
+    "nom": ["nom"],
+    "telephone": ["telephone", "numero"],
+    "bye": ["bye", "revoir"]
+}
+
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import azure.cognitiveservices.speech as speechsdk
+
 from Query import *
 from text_to_speech import *
+from constantes import *
+
 from random import randint
 import nltk # librairie qui permet de couper les phrases avec des tokens
+import difflib
 
 nltk.download('punkt')
 nltk.download('maxent_ne_chunker')
@@ -26,18 +30,8 @@ while 1:
 
         query = Query()
 
-        word_list = ["gueule", "bonjour", "hey", "hi", "yo", "salut", "age", "mail", "prenom", "ville", "adresse",
-                     "l'adresse", "habite", "naissance", "numero", "telephone", "nom", "astrologique", "signe",
-                     "bye", "au revoir", "exit", "va", "ça", "ca", "vas", "comment", "anniversaire", "l'anniversaire",
-                     "horoscope", "l'horoscope"]
         firstname_list = query.firstname_list()
 
-        possible_response = ["Bien sur que non", "Je ne pense pas ", "No!",
-                             "Arrete de poser des questions", "Qu'est-ce que j'en sais moi ?",
-                             "Je suis ton père", "Arrete de dire des conneries stp", "Tu sors ou je te sors?",
-                             "C'est qui la patronne?", "Nope", "Demandes à PL", "Demandes à Sumenia", "Plaît-il",
-                             "pffffff", ":confused:", ":snake:", ":eye:", ":octopus:"
-                             ]
 
         list_query = []  # initialisation de la liste des requêtes que l'on va effectuer
         firstname = ""  # init vide pour les demandes de prenoms non connu par la db
@@ -45,56 +39,53 @@ while 1:
 
         tokens = nltk.word_tokenize(result.text)  # permet de recupérer la réponse en tokens (mot par mot)
 
-        for elt in tokens:  # parcourir tous les tokens
-            if elt.capitalize() in firstname_list:  # si l'un des tokens est un prenom dans la liste des prenoms....
-                firstname = elt.capitalize()  # le prenom devient cet élèment
-            if elt.lower() in word_list:  # si un des tokens dans la liste des mots...
-                list_query.append(elt.lower())  # on l'ajoute dans la liste des requêtes à effectuer
+        for elt in tokens:
+            if elt.capitalize() in firstname_list:
+                firstname = elt.capitalize()
+            for k, v in word_dict.items():
+                if elt.lower() in v:
+                    list_query.append(k.lower())
 
-        for elt in list_query:  # on parcourt ttes les requ effectuées (les if...)
+        temp = 0
+        for elt in list_query:
+            if elt == "ça_va":
+                temp += 1
+        if temp ==1:
+            list_query.remove("ça_va")
+
+        list_query = set(list_query)
+        for elt in list_query:
             if elt == "nom":
                 answer += query.name(firstname) + '\n'
-            elif elt == "naissance":
+            elif elt == "date":
                 answer += query.date(firstname) + '\n'
-            elif elt == "anniversaire" or elt == "l'anniversaire":
+            elif elt == "anniversaire":
                 answer += query.anniversaire(firstname) + '\n'
-            elif elt == "horoscope" or elt == "l'horoscope":
+            elif elt == "horoscope":
                 answer += query.horoscope(firstname) + '\n'
-            elif elt == "ville" or elt == "habite" or elt == "adresse" or elt == "l'adresse":
+            elif elt == "adresse":
                 answer += query.city(firstname) + '\n'
-            elif elt == "numero" or elt == "telephone":
+            elif elt == "numero":
                 answer += query.number(firstname) + '\n'
             elif elt == "age":
                 answer += query.age(firstname) + '\n'
             elif elt == "mail":
                 answer += query.mail(firstname) + '\n'
-            elif elt == "astrologique" or elt == "signe":
+            elif elt == "signe":
                 answer += "Son signe est " + query.zodiac_sign(firstname) + '\n'
-            elif elt == "bye" or elt == "au revoir" or elt == "exit":
+            elif elt == "bye":
                 answer += "See you soon loser!"
-            elif elt == "bonjour" or elt == "hey" or elt == "yo" or elt == "salut" or elt == "hi":
+            elif elt == "salut":
                 answer += "Bonjour ! Je suis Chit-Chat (: "
-            elif elt == "gueule":
-                if firstname != "":
-                    if firstname == "Theo" or firstname == "Timothée":
-                        answer += "Non, j'ai trop de respect pour lui, c'est l'un de mes créateurs"
-                    elif firstname == "Caroline" or firstname == "Haikouhi":
-                        answer += "Non, j'ai trop de respect pour elle, c'est l'une de mes créatrices"
-                    else:
-                        answer += "Ta gueule {}".format(firstname)
-            elif elt == "va" or elt == "vas":
-                if "ca" in list_query or "ça" in list_query or "comment" in list_query:
-                    answer += "Je pète la forme"
+            elif elt == "ça_va":
+                answer += "Je pète la forme"
 
         if len(list_query) == 0:  # si liste des Query = 0...
 
-            index_response_picked = randint(0, len(
-                possible_response) - 1)  # on tire au sort un index entre 0 et nombre-1...
-            response = possible_response[index_response_picked]  # réponse choisie...
-            answer += response  # et ajoutée !
+            answer += "Je n'ai pas compris ce que vous vouliez"
 
         print(answer)
-        subscription_key = "mettre subscription key"
+        subscription_key = speech_key
         app = TextToSpeech(subscription_key, answer)
         app.get_token()
         app.save_audio()
