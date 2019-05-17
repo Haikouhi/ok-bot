0b612482bce272ecf14336ff82daf4e2f5b06093
Haikouhi <h.oroudjian@protonmail.com>
2019-05-17 13:38:01 +0200

    fixing some details

--- QueryClass.py
+++ QueryClass.py
@@ -224,59 +224,31 @@ class Query(): # gère toutes les req
         speak_answer(speech_key, "Enter city name : ")
 
 
-        # base_url variable to store url 
         base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-        # Give city name 
         city_name = ""
         result = speech_recognizer.recognize_once()
 
         if result.reason == speechsdk.ResultReason.RecognizedSpeech:
             city_name = result.text
             city_name = city_name[:-1]
-
-        # complete_url variable to store 
-        # complete url address 
+ 
         complete_url = base_url + "appid=" + api_key + "&q=" + city_name.capitalize() 
 
-        # get method of requests module 
-        # return response object 
         response = requests.get(complete_url) 
 
-        # json method of response object 
-        # convert json format data into 
-        # python format data 
         x = response.json() 
 
-        # Now x contains list of nested dictionaries 
-        # Check the value of "cod" key is equal to 
-        # "404", means city is found otherwise, 
-        # city is not found 
         if x["cod"] != "404": 
 
-            # store the value of "main" 
-            # key in variable y 
             y = x["main"] 
 
-            # store the value corresponding 
-            # to the "temp" key of y 
             current_temperature = y["temp"] 
-
-            # store the value corresponding 
-            # to the "pressure" key of y 
             current_pressure = y["pressure"] 
-
-            # store the value corresponding 
-            # to the "humidity" key of y 
             current_humidiy = y["humidity"] 
 
-            # store the value of "weather" 
-            # key in variable z 
             z = x["weather"] 
 
-            # store the value corresponding 
-            # to the "description" key at 
-            # the 0th index of z 
             weather_description = z[0]["description"] 
 
             return(" Temperature (in kelvin unit) = " +
